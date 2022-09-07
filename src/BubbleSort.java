5e20d936357d8b92465f81dd161bfc03fea3ca8b
shubh <shubh@10.12.38.90>
2022-09-07 22:48:15 +0530

    bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 5, 1, 4, 2, 8 };
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+	
+	}
+
+}
